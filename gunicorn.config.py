aec9323ee83cca201abde462f7eda7d9c5e0f9d4
Ubuntu <ubuntu@ip-172-31-9-167.ec2.internal>
2017-05-05 17:31:37 +0000

    reduce num of workers

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -2,4 +2,4 @@ bind='127.0.0.1:5000'
 errorlog = '/home/ubuntu/CMT-Text-Detection/logs/gunicorn-error.log'
 accesslog = '/home/ubuntu/CMT-Text-Detection/logs/gunicorn-access.log'
 loglevel = 'debug'
-workers = 17
+workers = 1
